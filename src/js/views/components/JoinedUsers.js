ede6b06b5ce2bb98b7b366ebe01732c57ad56c3f
solone313 <solone313@naver.com>
2019-12-26 19:07:38 +0900

    chatting ui

--- src/js/views/components/JoinedUsers.js
+++ src/js/views/components/JoinedUsers.js
@@ -24,6 +24,13 @@ class JoinedUsers extends React.Component{
         return (
             <div className='users'>
                 {this.joinedUsers}
+                <div className="chating_server">
+                    <div>
+                        <textarea id="chat-area" style={{width:"220px"}} rows="20" readOnly></textarea>
+                    </div>
+                    <input type="text" id="chat-message" style={{width:"220px"}}/>
+                    <input type="button" id="chat-send" value="send"/>
+                </div>
             </div>
         );
     }
